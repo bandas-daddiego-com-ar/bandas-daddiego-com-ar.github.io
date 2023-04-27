3bcde54c6986de9df352f362123b6a59d1c8fd3a
Lucas Daddiego <daddiego.lucas@gmail.com>
2023-04-26 21:08:31 -0300

    Add vite-pwa plugin

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,23 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import { VitePWA } from 'vite-plugin-pwa'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({ 
+      registerType: 'autoUpdate',
+      includeAssets: ['/icons/icon.png', '/icons/icon.svg'],
+      manifest: {
+        name: 'Metrónomo',
+        short_name: 'Metrónomo',
+        description: 'Metrónomo',
+        theme_color: '#0c0c0c0',
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
