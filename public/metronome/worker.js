51760cd6c9d34b95c573bb579f619b33ebf7a58f
Lucas Daddiego <daddiego.lucas@gmail.com>
2023-04-30 23:21:23 -0300

    improvements

--- public/metronome/worker.js
+++ public/metronome/worker.js
@@ -1,7 +1,5 @@
 let timerID;
 self.onmessage = (e) => {
-  if (e.data)
-    timerID = setInterval(() => postMessage(0), 25);
-  else
-    clearInterval(timerID);
-}
+  if (e.data) timerID = setInterval(() => postMessage(0), 25);
+  else clearInterval(timerID);
+};
